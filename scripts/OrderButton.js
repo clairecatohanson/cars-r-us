1d2b1fde2b5105870818fe10e906bacb98a7fa0b
Claire Hanson <clairecatohanson@gmail.com>
2023-12-04 11:52:58 -0600

    update handleOrderButtonClick() in OrderButton.js

--- scripts/OrderButton.js
+++ scripts/OrderButton.js
@@ -1,14 +1,12 @@
 import { postToAPI } from './TransientState.js'
 
-document.addEventListener("click", (clickEvent) => {
+const handleOrderButtonClick = (clickEvent) => {
     if (clickEvent.target.id==="button__order") {
         postToAPI()
     }
-})
+}
 
 export const OrderButtonHTML = () => {
-    const html = `
-        <button id="button__order">Place Custom Car Order</button>
-    `
-    return html
+    document.addEventListener("click", handleOrderButtonClick)
+    return `<button id="button__order">Place Custom Car Order</button>`
 }
